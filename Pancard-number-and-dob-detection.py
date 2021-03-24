42203029c0ff2cbc63b21ca0480edbf88915596b
SriVathsav9 <76102208+SriVathsav9@users.noreply.github.com>
2021-03-24 23:38:07 +0530

    Update Pancard-number-and-dob-detection.py

--- Pancard-number-and-dob-detection.py
+++ Pancard-number-and-dob-detection.py
@@ -5,23 +5,23 @@ from pytesseract import Output
 tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 from PIL import Image
 
-
 import cv2
 
 import imutils
 
-
-img = cv2.imread('3657993.jpg')
+img = cv2.imread('3648476-1.jpg')
 
 a=int(input("Enter the angle to be rotated"))  
-
-
   
 Rotated_image = imutils.rotate(img, a)
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def remove_noise(image):
+    return cv2.medianBlur(image,3)
+gray = get_grayscale(Rotated_image)
+noise=remove_noise(gray)
+d = tess.image_to_data(noise, output_type=Output.DICT)
 
-
-d = tess.image_to_data(Rotated_image, output_type=Output.DICT)
-keys = list(d.keys())
 
 date_pattern = '^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$'
 card_number = "[A-Z]{5}[0-9]{4}[A-Z]{1}"
@@ -40,12 +40,5 @@ for i in range(n_boxes):
 	        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
 	        img = cv2.rectangle(Rotated_image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-
-
 cv2.imshow('PANCARD NUMBER AND DATE OF BIRTH DETECTION', img)
-
-
-
-
-
 cv2.waitKey(0)
